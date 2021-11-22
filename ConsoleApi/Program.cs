b7823e624448e643155889bc82dfe4b21808beb6
RaphaelBento <57066063+RaphaelBento@users.noreply.github.com>
2021-11-22 12:41:01 -0300

    Commit 22/11/2021
    Aplicação console em c# para consultar api de ceps

---
+++ ConsoleApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("bem vindo ao sistema de consulta de ceps por API");
+            Console.WriteLine("Insira o cep que deseja pesquisar  somente numeros sem traço ou ponto!!");
+            var cep = Console.ReadLine();
+            HttpClient client = new HttpClient { BaseAddress = new Uri("https://viacep.com.br/ws/" + cep + "/json/") };
+
+            var response = await client.GetAsync("");
+            var clienteresposta = await response.Content.ReadAsStringAsync();
+
+            var informacaoes = JsonConvert.DeserializeObject<GuardaInfo>(clienteresposta);
+
+
+            Console.WriteLine($"Bairro:{informacaoes.bairro} \n Cep:{informacaoes.cep}" +
+                $"\n DDD:{informacaoes.ddd}" +
+                $"\n IBGE: {informacaoes.ibge} \n Localidade:{informacaoes.localidade}" +
+                $"\n Logradouro:{informacaoes.logradouro} \n Siafi:{informacaoes.siafi} \n Estado:{informacaoes.uf}");
+            Console.ReadLine();
+        }
+    }
+}
